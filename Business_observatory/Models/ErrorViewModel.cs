b8b85d60f2313ee8c351a3cc993262d997cf7a02
MARCELO CONDORI APALA <cam5000810@est.univalle.edu>
2023-06-02 21:20:07 -0400

    UI Frontend Backend

--- Business_observatory/Models/ErrorViewModel.cs
+++ Business_observatory/Models/ErrorViewModel.cs
@@ -1,9 +1,9 @@
 namespace Business_observatory.Models
 {
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
 }
